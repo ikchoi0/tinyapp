637bbe5a96b15dc3a17a638935114a4ce30ac675
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-16 19:57:49 -0400

    add delete button and delete  route

--- express_server.js
+++ express_server.js
@@ -18,7 +18,11 @@ function generateRandomString() {
   }
   return result;
 }
-
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req.params.shortURL);
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+}); 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -41,13 +45,13 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
   let generatedURL = generateRandomString();
   urlDatabase[generatedURL] = req.body.longURL;
   res.redirect(`/urls/${generatedURL}`);         // Respond with 'Ok' (we will replace this)
@@ -56,6 +60,7 @@ app.get("/u/:shortURL", (req, res) => {
   // const longURL = ...
   res.redirect(urlDatabase[req.params.shortURL]);
 });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}`);
 });
